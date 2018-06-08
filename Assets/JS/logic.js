993a92f1bc18a1181361528762ca54fd0da25fea
Kevin Murtaugh <35198999+Kevin-Murtaugh@users.noreply.github.com>
2018-06-08 18:35:08 -0400

    delete old versionsm, connect remainder

--- Assets/JS/logic.js
+++ Assets/JS/logic.js
@@ -3,19 +3,19 @@
 // <script src="https://www.gstatic.com/firebasejs/4.12.1/firebase.js"></script>
 
 //new firebase code
-  // Initialize Firebase (new vers 4/11/18... 11:02am)
+//Initialize Firebase (new vers 4/11/18... 11:02am)
+
 var config = {
-   apiKey: "AIzaSyDc_3A31WIH08AXrGk8QusY5YWfX-qKnEI",
-   authDomain: "traintracker-94b09.firebaseapp.com",
-   databaseURL: "https://traintracker-94b09.firebaseio.com",
-   projectId: "traintracker-94b09",
-   storageBucket: "",
-   messagingSenderId: "544426864840"
+  apiKey: "AIzaSyDc_3A31WIH08AXrGk8QusY5YWfX-qKnEI",
+  authDomain: "traintracker-94b09.firebaseapp.com",
+  databaseURL: "https://traintracker-94b09.firebaseio.com",
+  projectId: "traintracker-94b09",
+  storageBucket: "",
+  messagingSenderId: "544426864840"
 };
- firebase.initializeApp(config);
+firebase.initializeApp(config);
 
 var database = firebase.database();
-
 
 // 2. Button for adding trains
 
@@ -23,10 +23,21 @@ $("#add-train-btn").on("click", function(event) {
   event.preventDefault();
 
   // Grabs user input
-  var trainName = $("#train-name-input").val().trim();
-  var trainDest = $("#destination-input").val().trim();
-  var firstTrainTime = moment($("#first-train-time-input").val().trim(), "hh:mm").format("X");
-  var trainFrequency = $("#frequency-input").val().trim();
+  var trainName = $("#train-name-input")
+    .val()
+    .trim();
+  var trainDest = $("#destination-input")
+    .val()
+    .trim();
+  var firstTrainTime = moment(
+    $("#first-train-time-input")
+      .val()
+      .trim(),
+    "hh:mm"
+  ).format("X");
+  var trainFrequency = $("#frequency-input")
+    .val()
+    .trim();
 
   // Creates local "temporary" object for holding train data
   var trainTracker = {
@@ -47,7 +58,6 @@ $("#add-train-btn").on("click", function(event) {
   // $("#destination-input").val("");
   // $("#first-train-time-input").val("");
   // $("#frequency-input").val("");
-
 });
 
 // 3. Create Firebase event for adding train to the database and a row in the html when a user adds an entry
@@ -62,22 +72,31 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
   // Prettify the train data
   var trainStart = moment.unix(firstTrainTime).format("hh:mm A");
 
-
-  // check if current time exceeds arrival time & make sure minutes away is a positive number 
+  // check if current time exceeds arrival time & make sure minutes away is a positive number
   var unixFirsTrainTime = moment.unix(firstTrainTime, "X");
   var now = moment();
   var trainTimeRemaining = 0;
-  if(unixFirsTrainTime > now) {
+  if (unixFirsTrainTime > now) {
     trainTimeRemaining = unixFirsTrainTime.diff(moment(), "minutes");
-  }
-  else {
-    while(unixFirsTrainTime < now) {
-      unixFirsTrainTime.add(parseInt(trainFrequency), 'm');
+  } else {
+    while (unixFirsTrainTime < now) {
+      unixFirsTrainTime.add(parseInt(trainFrequency), "m");
     }
     trainTimeRemaining = unixFirsTrainTime.diff(moment(), "minutes");
-  }  
+  }
 
   // Add each train's data into the table
-  $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDest + "</td><td>" +
-  unixFirsTrainTime.format("hh:mm A") + "</td><td>" + trainFrequency + "</td><td>" + trainTimeRemaining + "</td><td>");
+  $("#train-table > tbody").append(
+    "<tr><td>" +
+      trainName +
+      "</td><td>" +
+      trainDest +
+      "</td><td>" +
+      unixFirsTrainTime.format("hh:mm A") +
+      "</td><td>" +
+      trainFrequency +
+      "</td><td>" +
+      trainTimeRemaining +
+      "</td><td>"
+  );
 });
--- README.md
+++
@@ -1,4 +0,0 @@
-# Train-Time
-UCF Full Stack Bootcamp Week 4 homework, adding Firebase DB to our repertoire'
-
-Additional details, challenges and victories will be explained here in the future (4/9/2018)
